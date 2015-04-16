0a61c654a7e6ec17460748811df00000de75a8d2
smasover <masover@berkeley.edu>
2015-04-16 15:26:34 -0700

    startup app as other than root; still needs some polishing

---
+++ hackfsm8/scripts/app-startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/etc/init.d/mysql start
+adduser --disabled-password --gecos '' railsusr
+cd /var/tmp; chown -R railsusr hackfsm
+su -m railsusr -c /var/tmp/scripts/app-startup2.sh
---
+++ hackfsm8/scripts/app-startup2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PATH=/usr/local/rvm/gems/ruby-2.2.0/bin:/usr/local/rvm/gems/ruby-2.2.0@global/bin:/usr/local/rvm/rubies/ruby-2.2.0/bin:/usr/local/rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+GEM_PATH=/usr/local/rvm/gems/ruby-2.2.0:/usr/local/rvm/gems/ruby-2.2.0@global
+cd /var/tmp/hackfsm
+rails s
+
